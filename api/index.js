90a785ba37badb7f57c86a0e092816a5c5f3b748
cufardixx <facundopicia@gmail.com>
2024-05-22 07:28:12 -0300

    agregado de boton delete ya funcionando

--- api/index.js
+++ api/index.js
@@ -1,58 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-const PORT = 3000
-let arrayTransacciones = []
+const app = express();
+const PORT = 3000;
+let arrayTransacciones = [];
 
+// Procesar datos enviados mediante formularios HTML
+app.use(express.urlencoded({ extended: true }));
 
-// Middleware para analizar el cuerpo de las solicitudes entrantes como JSON
-app.use(bodyParser.json());
+// Middleware para analizar datos JSON entrantes en las solicitudes HTTP
+app.use(express.json({ type: "*/*" }));
 
-//Procesar datos enviados mediante formularios HTML 
-app.use(express.urlencoded({
-        extended: true })
-    );
-
-//middleware para analizar datos JSON entrantes en las solicitudes HTTP
-app.use(express.json({
-     type: "*/*" })
-    );
-
-//permitir que tu servidor responda a solicitudes desde un dominio diferente al de la propia aplicación
+// Permitir que tu servidor responda a solicitudes desde un dominio diferente al de la propia aplicación
 app.use(cors());
 
+// Crear rutas
+app.get('/transaccion', (req, res) => {
+    res.json(arrayTransacciones); // Enviar JSON directamente
+});
 
-
-//Crear rutas
-app.get('/transaccion', (req, res)=>{
-    res.send(JSON.stringify(arrayTransacciones))
-})
-
-
-
-app.post('/transaccion', (req, res)=>{
-    //console.log(req.body);
-    let respuestaTransaccion = req.body
-    let desc = respuestaTransaccion.ID;
-    console.log(desc);
-
-    arrayTransacciones.push(respuestaTransaccion)
+app.post('/transaccion', (req, res) => {
+    let respuestaTransaccion = req.body;
+    arrayTransacciones.push(respuestaTransaccion);
     console.log(arrayTransacciones);
-    res.send("OK!")
-})
+    res.send("OK!");
+});
 
+app.delete('/transaccion/:id', (req, res) => {
+    const id = req.params.id;
+    arrayTransacciones = arrayTransacciones.filter(transaccion => transaccion.id !== id);
+    res.json(arrayTransacciones); // Devolver el array actualizado como JSON
+    console.log(arrayTransacciones);
+});
 
-app.delete('/transaccion', (req,res)=>{
-    const id = req.params.ID
-})
+// GET -> miPc/transaccion -> Devolver las Transacciones
+// POST -> miPc/transaccion con una {Transaccion} -> Guardar la transaccion
 
-
-
-// GET -> miPc/transaccion -> Devolve las Trasnsacciones
-//POST -> miPc/trasacciones con una {Transaccion} -> Guarda la transaccion
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Servidor Express en ejecución en el puerto ${PORT}`);
-  });
+});
